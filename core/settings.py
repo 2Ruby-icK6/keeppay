f7460914ca3c0867ceffb1297db5d5d81a7b935f
D1zputed <asaphdelcoro987@gmail.com>
2024-10-09 22:52:43 +0800

    modified allowed host

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 
-                 "https://d1zputed.pythonanywhere.com/"]
+                 "d1zputed.pythonanywhere.com"]
 
 # Application definition
 
